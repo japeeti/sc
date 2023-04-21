c27b687b1d7a2a1201a30220bf6567c8f3cb4dc8
japeetifromtahiti <japeetifromtahiti@gmail.com>
2023-04-20 17:05:48 -0700

    Fixed bugs in the code

--- etherstore.sol
+++ etherstore.sol
@@ -2,17 +2,18 @@ contract EtherStore {
     mapping(address => uint) public balances;
 
     function deposit() public payable {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
         balances[msg.sender] += msg.value;
     }
 
     function withdraw() public {
         uint bal = balances[msg.sender];
-        require(bal > 0);
+        require(bal > 0, "Insufficient balance");
+
+        balances[msg.sender] = 0;
 
         (bool sent, ) = msg.sender.call{value: bal}("");
         require(sent, "Failed to send Ether");
-
-        balances[msg.sender] = 0;
     }
 
     // Helper function to check the balance of this contract
--- wallet.sol
+++ wallet.sol
@@ -5,8 +5,14 @@ contract Wallet {
         owner = msg.sender;
     }
 
-    function transfer(address payable _to, uint _amount) public {
-        require(tx.origin == owner, "Not owner");
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function transfer(address payable _to, uint _amount) public onlyOwner {
+        require(_amount > 0, "Transfer amount must be greater than 0");
+        require(address(this).balance >= _amount, "Insufficient balance in wallet");
 
         (bool sent, ) = _to.call{value: _amount}("");
         require(sent, "Failed to send Ether");
